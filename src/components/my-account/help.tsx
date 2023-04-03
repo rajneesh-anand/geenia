cefa4aeecba11d6e887b291c294e7e79934dd00b
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2023-04-03 14:53:12 +0530

    react 18

--- src/components/my-account/help.tsx
+++ src/components/my-account/help.tsx
@@ -2,7 +2,7 @@ import Layout from "@components/layout";
 import Heading from "@components/ui/heading";
 import { useTranslation } from "next-i18next";
 
-import Accordion from "@components/ui/accordion";
+// import Accordion from "@components/ui/accordion";
 import { help } from "@settings/help-setting";
 
 export default function HelpCenter() {
@@ -11,13 +11,13 @@ export default function HelpCenter() {
     <>
       <Heading variant="titleLarge">{t("text-account-details-help")}</Heading>
       <div className="flex flex-col pt-6 2xl:pt-8">
-        {help?.map((item, index) => (
+        {/* {help?.map((item, index) => (
           <Accordion
             key={`${item.title}-${index}`}
             item={item}
             translatorNS="help"
           />
-        ))}
+        ))} */}
       </div>
     </>
   );
